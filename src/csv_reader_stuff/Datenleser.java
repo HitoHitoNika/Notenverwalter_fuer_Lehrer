60211d1f35304e4ec490c74f44a55882bb959c7b
HitoHitoNika <mbalthasar0@gmail.com>
2023-04-27 23:52:59 +0200

    file zu csvFile umbenannt

--- src/csv_reader_stuff/Datenleser.java
+++ src/csv_reader_stuff/Datenleser.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Datenleser {
     private String projectPath = System.getProperty("user.dir");
-    private File file;
+    private File csvFile;
     private FileReader fr;
     private BufferedReader csvReader ;
     
@@ -16,12 +16,12 @@ public Datenleser() throws FileNotFoundException{
 public void setFilePath(String Fach) throws IOException{               //Bei Auswahl auf ein Fach soll dieses hier hin übergeben werden
     switch(Fach){
     case "Deutsch":                                                 
-        file = new File(projectPath+"/CSV_Dateien/Deutsch.csv");      
+        csvFile = new File(projectPath+"/CSV_Dateien/Deutsch.csv");      
         break;
     }
 }
 public void initReader() throws FileNotFoundException{     
-    fr = new FileReader(file);
+    fr = new FileReader(csvFile);
     csvReader = new BufferedReader(fr);
 }
 
