585387cb508dbd985692dd9c0602e6b5c65417bb
HitoHitoNika <mbalthasar0@gmail.com>
2023-05-02 22:26:05 +0200

    Aufruf des GUIs auf App umgelagert

--- src/start/App.java
+++ src/start/App.java
@@ -1,10 +1,28 @@
 package start;
 
+import java.awt.EventQueue;
+
 import gui.KlassenAuswaehlen_Frame;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        KlassenAuswaehlen_Frame test = new KlassenAuswaehlen_Frame();
-        test.main(args);
-    }
+    //Starten des GUIs
+    public static void main (String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            //Erstellt einen Thread für das GUI
+          @Override
+          public void run() {
+            try {
+            //Objekt des GUIs wird instanziiert 
+              KlassenAuswaehlen_Frame frame = new KlassenAuswaehlen_Frame();
+            //GUI wird sichtbar gemacht
+              frame.setVisible(true);
+            //Da im Programm mehrere Exceptions passieren könnten, werden diese hier abgefangen
+            } catch (Exception e) {
+              e.printStackTrace();
+              // Zur einfacheren Nachverfolgung falls Fehler auftreten
+              System.out.println("Main()=>Run() Aufruf");
+            }
+          }
+        });
+      }
 }
