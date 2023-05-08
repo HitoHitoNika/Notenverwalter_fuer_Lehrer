3c151c8d80ab0a77dd617a97145e08daa8554527
HitoHitoNika <mbalthasar0@gmail.com>
2023-05-08 15:25:05 +0200

    Test added

---
+++ src/test/KlassenAuswaehlen_FrameTest.java
@@ -0,0 +1,16 @@
+package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Test;
+import gui.KlassenAuswaehlen_Frame;
+
+public class KlassenAuswaehlen_FrameTest {
+    @Test
+    public void testKonstruktor() throws IOException{
+        KlassenAuswaehlen_Frame test = new KlassenAuswaehlen_Frame();
+    }
+    
+}
