c888401c9a26a90580be03a42c46207b89ee51ef
HitoHitoNika <mbalthasar0@gmail.com>
2023-06-02 14:21:11 +0200

    Beginn Vorbereitung Webservice

--- src/start/MainGUI.java
+++ src/start/MainGUI.java
@@ -9,7 +9,7 @@ import OneInAll_GUI.Dashboard_Gui;
  * Der Sinn der Main-Klasse ist das auslagern von dem intialen Aufruf des Programms
  * Hier wird ein Thread für das GUI erstellt und dieses erstellt und sichtbar gemacht
  */
-public class Main {
+public class MainGUI {
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
