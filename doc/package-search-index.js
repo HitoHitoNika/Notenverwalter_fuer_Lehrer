b556a8b88624740607588859d4c4e4693220f10f
HitoHitoNika <mbalthasar0@gmail.com>
2023-06-11 18:39:41 +0200

    Neue Docs added

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"csv_reader_stuff"},{"l":"OneInAll_GUI"},{"l":"school_attributes"},{"l":"start"},{"l":"test"}];updateSearchResults();
